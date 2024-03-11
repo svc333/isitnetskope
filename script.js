b6edb45d30109644283f8548e2eed105bb03dd21
svc333 <36961991+svc333@users.noreply.github.com>
2024-03-11 17:50:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const spinButton = document.getElementById("spinButton");
 const spinWheel = document.getElementById("spinWheel");
-const options = ["YES", "NO"];
+const options = ["YES", "YES"];
 
 spinButton.onclick = function() {
     const spinTime = Math.floor(Math.random() * 2000) + 1000; // Random spin time between 1000 and 3000 ms
